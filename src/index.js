28c20843e419fb0cb95ee74fb9fb2ff3a781579c
Korotenko Alex <alexey.korotenko90@gmail.com>
2022-05-08 20:59:34 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // <BrowserRouter>
   <React.StrictMode>
+    {/* <Routes> */}
+    {/* <Route path="/" element={<App />} /> */}
     <App />
+    {/* </Routes> */}
   </React.StrictMode>
+  // </BrowserRouter>
 );
